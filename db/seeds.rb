8352d5505f6494d4eb6bbaab58acae5eab0c02da
Frederick Wong <frederick.kf.wong@gmail.com>
2016-03-29 15:54:01 +0900

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-snacks = Snack.create([{
+snacks = Snack.create([
+  {
     name: 'Pocky',
     description: "The original Pocky! Introduced in 1966 as 
     'the world's first chocolate stick snack,' 
@@ -16,4 +17,29 @@ snacks = Snack.create([{
     Pocky Chocolate is the perfect every-day, 
     all-occasion chocolate snack.",
     code: 12312
-  }])
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  }
+
+
+])
