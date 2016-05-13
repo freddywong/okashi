6d7381ff05041343e145457de0c9952e9fbb8738
Frederick Wong <frederick.kf.wong@gmail.com>
2016-05-13 17:24:41 +0900

    Integrated circle loader

--- app/assets/javascripts/shared/infinite_scroll.js
+++ app/assets/javascripts/shared/infinite_scroll.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   if ($('.pagination').length) {
     $(window).scroll(function() {
       var url = $('.pagination .next_page').attr('href');
-      debugger;
+      
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        // $('.snacks').append('<%= escape_javascript render(partial: "snack", collection: @snacks) %>');
-        // $('.pagination').append("<%= escape_javascript("#{render :partial => 'components/big_colorful_circle_loader'}").html_safe %>");
-        // $("#comments_<%=@comment.post.id %>").append("<%= escape_javascript("#{render :partial => 'posts/comment', :locals => { :comment => @comment }}").html_safe %>");
-        $('.pagination').append("")
+        
+        $('.pagination').hide();
+        $('.big-colorful-circle-loader').show();
+
         return $.getScript(url);
       }
     });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "snacks#index"
+
+  namespace :shared do
+    namespace :components do
+      get 'big_colorful_circle_loader'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
