bb93db272c6c88fe37e6af21437e162b32fe94a9
Frederick Wong <frederick.kf.wong@gmail.com>
2016-05-13 10:40:52 +0900

    Added extra seeds of snacks

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,215 @@ snacks = Snack.create([
     that's designed to give you a lift and wake you up, this 
     popular gum is loaded with caffeine. ",
     code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Pocky',
+    description: "The original Pocky! Introduced in 1966 as 
+    'the world's first chocolate stick snack,' 
+    Pocky Chocolate is a tasty, crispy cookie biscuit stick 
+    dipped in rich, dark chocolate cream with a unique 'handle.' 
+    Delicious alone or with coffee, tea, milk, or wine, 
+    Pocky Chocolate is the perfect every-day, 
+    all-occasion chocolate snack.",
+    code: 12312
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Pocky',
+    description: "The original Pocky! Introduced in 1966 as 
+    'the world's first chocolate stick snack,' 
+    Pocky Chocolate is a tasty, crispy cookie biscuit stick 
+    dipped in rich, dark chocolate cream with a unique 'handle.' 
+    Delicious alone or with coffee, tea, milk, or wine, 
+    Pocky Chocolate is the perfect every-day, 
+    all-occasion chocolate snack.",
+    code: 12312
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Pocky',
+    description: "The original Pocky! Introduced in 1966 as 
+    'the world's first chocolate stick snack,' 
+    Pocky Chocolate is a tasty, crispy cookie biscuit stick 
+    dipped in rich, dark chocolate cream with a unique 'handle.' 
+    Delicious alone or with coffee, tea, milk, or wine, 
+    Pocky Chocolate is the perfect every-day, 
+    all-occasion chocolate snack.",
+    code: 12312
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Pocky',
+    description: "The original Pocky! Introduced in 1966 as 
+    'the world's first chocolate stick snack,' 
+    Pocky Chocolate is a tasty, crispy cookie biscuit stick 
+    dipped in rich, dark chocolate cream with a unique 'handle.' 
+    Delicious alone or with coffee, tea, milk, or wine, 
+    Pocky Chocolate is the perfect every-day, 
+    all-occasion chocolate snack.",
+    code: 12312
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Pocky',
+    description: "The original Pocky! Introduced in 1966 as 
+    'the world's first chocolate stick snack,' 
+    Pocky Chocolate is a tasty, crispy cookie biscuit stick 
+    dipped in rich, dark chocolate cream with a unique 'handle.' 
+    Delicious alone or with coffee, tea, milk, or wine, 
+    Pocky Chocolate is the perfect every-day, 
+    all-occasion chocolate snack.",
+    code: 12312
+  },
+  {
+    name: 'Hello Panda',
+    description: "A bite size cookie baked and then filled with 
+    chocolate, vanilla and strawberry créme. 
+    Each cookie contains a drawing of the cute Hello Panda 
+    enjoying various sports and boating. Look for all 24 drawings.",
+    code: 12313
+  },
+  {
+    name: 'Pretz',
+    description: "The savory, creamless version of Pocky, Pretz 
+    are also made by Glico, presumably in the exact same 
+    long-skinny-cylindrical biscuit factory.",
+    code: 12314
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
+  },
+  {
+    name: 'Black Black Gum',
+    description: "A great chewing gum with a strong, minty flavor 
+    that's designed to give you a lift and wake you up, this 
+    popular gum is loaded with caffeine. ",
+    code: 12315
   }
+
 
 
 ])
