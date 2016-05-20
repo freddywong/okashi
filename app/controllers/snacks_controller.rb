aae651f425f49a09a49cd8e57c8a6a6a41704d40
Frederick Wong <frederick.kf.wong@gmail.com>
2016-05-20 14:33:06 +0900

    Refactored the way to route in the snacks controller

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,9 +1,26 @@
 class SnacksController < ApplicationController
+  before_action :snack_change
+
   def index
     @snacks = Snack.all
+    get_snack_category
+    paginate_snacks
+  end
+
+  private
+
+  def paginate_snacks
+    @snacks = @snacks.paginate(:page => params[:page], :per_page => 20)
+  end
+
+  def get_snack_category
     if params[:category]
       @snacks = @snacks.where(category: params[:category])
     end
-    @snacks = @snacks.paginate(:page => params[:page], :per_page => 20)
   end
+
+  def snack_change
+    @snack_change = params[:snack_change]
+  end
+
 end
