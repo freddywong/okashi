0fdcdc618e5aab69b2a737684e322b25b493b357
Frederick Wong <frederick.kf.wong@gmail.com>
2016-05-21 20:18:22 +0900

    If the user is not an admin they will be unauthorised to access certain pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def after_sign_in_path_for(resource)
+    current_user.admin? ? new_admin_snack_url : root_url
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
